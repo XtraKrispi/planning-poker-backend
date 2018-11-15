b7f169053239a3d6f4457cccbcf66f0b392dea14
Michael Gold <mgold@spscommerce.com>
2018-11-15 09:18:06 -0500

    Refactor to use player Id coming in querystring

--- src/Main.hs
+++ src/Main.hs
@@ -5,8 +5,10 @@
 module Main where
 
 import Control.Exception (finally)
-import Control.Monad (forever, unless)
+import Control.Monad (forever, join)
 import Data.Either (either)
+import Data.Foldable (traverse_)
+import GHC.Generics
 import qualified Data.Text                      as T
 import qualified Network.Wai                    as WAI
 import qualified Network.Wai.Handler.Warp       as W
@@ -18,11 +20,16 @@ import qualified Control.Monad.STM              as STM
 import qualified Control.Concurrent.STM.TVar    as TVar
 import qualified Data.Attoparsec.Text           as P
 import qualified Data.ByteString.Lazy           as BS
+import qualified Data.ByteString.Char8          as C8
 import qualified Data.Text.Encoding             as Enc
 import qualified Data.Aeson                     as A
-import GHC.Generics
+import qualified Network.URI                    as URI
+import qualified Data.Map                       as Map
 
-newtype Player = Player { playerName :: T.Text }
+data Player = Player { playerId           :: T.Text
+                     , playerName         :: T.Text
+                     , playerSelectedCard :: Maybe Card 
+                     }
   deriving (Show, Generic)
 
 instance A.ToJSON Player where
@@ -30,17 +37,28 @@ instance A.ToJSON Player where
 
 instance A.FromJSON Player
 
-mkPlayer :: T.Text -> Player
-mkPlayer name = Player { playerName = name }
+type Card = Int
 
-type PlayerConnection = (Int, WS.Connection, Player)
+type PlayerId = T.Text
 
-data IncomingMessage = PlayerConnected T.Text 
-                     | PlayersRequest  
+mkPlayer :: PlayerId -> T.Text -> Player
+mkPlayer pId name = Player {  playerId           = pId
+                            , playerName         = name
+                            , playerSelectedCard = Nothing }
+
+type Game = Map.Map PlayerId PlayerConnection
+
+type PlayerConnection = (WS.Connection, Player)
+
+data IncomingMessage = 
+    PlayerConnected T.Text 
+  | PlayersRequest 
+  | PlayerSelectedCard Card 
   deriving (Show)
 
-data OutgoingMessage = AllPlayers [Player]
-                     | NewPlayer Player
+data OutgoingMessage = 
+    AllPlayers [Player]
+  | NewPlayer Player
   deriving (Show)
 
 toJson :: A.ToJSON a => a -> T.Text
@@ -51,45 +69,62 @@ convertMessage (NewPlayer player) = "PLAYERCONNECTED:" <> toJson player
 convertMessage (AllPlayers players) = "PLAYERS:" <> toJson players
 
 main :: IO ()
-main = do
-  conns <- TVar.newTVarIO []
-  nextId <- TVar.newTVarIO 1
-  W.run 8080 (app conns nextId)
+main = TVar.newTVarIO Map.empty >>= W.run 8080 . app
 
-app :: TVar.TVar [PlayerConnection] -> TVar.TVar Int -> WAI.Application
-app conns nextId = WWS.websocketsOr WSC.defaultConnectionOptions wsApp backupApp
+app :: TVar.TVar Game -> WAI.Application
+app conns = WWS.websocketsOr WSC.defaultConnectionOptions wsApp backupApp
   where
     wsApp :: WS.ServerApp
-    wsApp pending_conn = do
-      conn <- WSC.acceptRequest pending_conn
-      putStrLn "Connected"
-      connId <- STM.atomically $ do
-        nextId' <- TVar.readTVar nextId
-        TVar.writeTVar nextId (nextId' + 1)
-        return nextId'        
-      WS.forkPingThread conn 30
-      WS.sendTextData conn ("Hello, client!" :: T.Text)
-      finally (processMessages (connId, conn) conns) (onDisconnect connId conns)      
+    wsApp pending_conn@(WSC.PendingConnection _ req _ _) = do      
+      let uri = URI.parseURI (show . WS.requestPath $ req)      
+      case uri of
+        Nothing -> do
+          _ <- WSC.rejectRequest pending_conn "Not a valid url in request"
+          return ()
+        Just uri' ->
+          case getUserId uri' of
+            Nothing -> do
+              _ <- WSC.rejectRequest pending_conn "User ID not specified"
+              return ()
+            Just pId -> do
+              conn <- WSC.acceptRequest pending_conn
+              putStrLn "Connected"
+              WS.forkPingThread conn 30
+              WS.sendTextData conn ("Hello, client!" :: T.Text)
+              playerConnected (pId, conn) pId conns
+              finally (processMessages (pId, conn) conns) (onDisconnect pId conns)      
     backupApp :: WAI.Application
     backupApp _ respond = respond $ WAI.responseLBS HTTP.status400 [] "Not a websocket request"
+    getUserId :: URI.URI -> Maybe T.Text
+    getUserId = fmap Enc.decodeUtf8 
+              . join 
+              . Map.lookup "uid" 
+              . Map.fromList 
+              . HTTP.parseQuery 
+              . C8.pack 
+              . URI.uriQuery
 
-processMessages :: (Int, WS.Connection) -> TVar.TVar [PlayerConnection] -> IO ()
-processMessages (connId, conn) conns = 
+processMessages :: (PlayerId, WS.Connection) -> TVar.TVar Game -> IO ()
+processMessages (pId, conn) conns = 
   forever $ 
     P.parseOnly convert . getMessageText <$> WS.receiveDataMessage conn >>=
       either  (\_ -> return ()) 
               (\case
-                  PlayerConnected name -> do
-                    let player = mkPlayer name
-                    STM.atomically $ TVar.modifyTVar conns ((:) (connId, conn, player))
-                    conns' <- TVar.readTVarIO conns
-                    broadcastToAllBut connId (convertMessage $ NewPlayer player) conns'
-                    sendAllPlayersBut conn connId conns'
-                  PlayersRequest -> TVar.readTVarIO conns >>= sendAllPlayersBut conn connId) 
+                  PlayerConnected name -> playerConnected (pId, conn) name conns
+                  PlayersRequest -> TVar.readTVarIO conns >>= sendAllPlayersBut conn pId
+                  PlayerSelectedCard card -> STM.atomically $ selectCard card pId conns)
 
-getAllPlayersBut :: Int -> [PlayerConnection] -> [Player]
-getAllPlayersBut i = 
-  fmap (\(_, _, p) -> p) . filter (\(i', _, _) -> i' /= i)
+playerConnected :: (PlayerId, WS.Connection) -> T.Text -> TVar.TVar Game -> IO ()
+playerConnected (pId, conn) name conns = do
+  let player = mkPlayer pId name
+  STM.atomically $ TVar.modifyTVar conns $ Map.insert pId (conn, player)
+  conns' <- TVar.readTVarIO conns
+  broadcastToAllBut pId (convertMessage $ NewPlayer player) conns'
+  sendAllPlayersBut conn pId conns'
+                  
+getAllPlayersBut :: PlayerId -> Game -> [Player]
+getAllPlayersBut pId = 
+  fmap (\(_, p) -> p { playerSelectedCard = Nothing}) . Map.elems . Map.filterWithKey (\i' _ -> pId /= i') 
 
 getMessageText :: WS.DataMessage -> T.Text
 getMessageText msg =
@@ -98,28 +133,32 @@ getMessageText msg =
               WS.Binary bs -> bs
   in Enc.decodeUtf8 . BS.toStrict $ bin
 
-sendAllPlayersBut :: WS.Connection -> Int -> [PlayerConnection] -> IO ()
-sendAllPlayersBut conn connId = WS.sendTextData conn . convertMessage . AllPlayers . getAllPlayersBut connId
+sendAllPlayersBut :: WS.Connection -> PlayerId -> Game -> IO ()
+sendAllPlayersBut conn pId = WS.sendTextData conn . convertMessage . AllPlayers . getAllPlayersBut pId
 
+broadcastToAll :: T.Text -> Game -> IO ()
+broadcastToAll msg = traverse_ (\(conn, _) -> WS.sendTextData conn msg)
 
-broadcastToAll :: T.Text -> [PlayerConnection] -> IO ()
-broadcastToAll msg = 
-  mapM_ (\(i, conn, _) -> do
-    putStrLn $ "Sending message to: " <> show i
-    WS.sendTextData conn msg)
+broadcastToAllBut :: PlayerId -> T.Text -> Game -> IO ()
+broadcastToAllBut pId msg game = 
+  broadcastToAll msg (Map.filterWithKey (\i' _ -> pId /= i') game)
 
-broadcastToAllBut :: Int -> T.Text -> [PlayerConnection] -> IO ()
-broadcastToAllBut i msg = 
-  mapM_ (\(i', conn, _) -> unless (i == i') $ WS.sendTextData conn msg)  
-
-onDisconnect :: Int -> TVar.TVar [PlayerConnection] -> IO ()
-onDisconnect conn conns = do
-  STM.atomically $ TVar.modifyTVar conns (filter (\(i, _, _) -> (i /= conn)))
+onDisconnect :: PlayerId -> TVar.TVar Game -> IO ()
+onDisconnect pId conns = do
+  STM.atomically $ TVar.modifyTVar conns (Map.delete pId)
   putStrLn "Disconnect"
   return ()
 
+selectCard :: Card -> PlayerId -> TVar.TVar Game -> STM.STM ()
+selectCard card pId conns = do
+  conns' <- TVar.readTVar conns
+  let adjusted = Map.adjust (\(c, p) -> (c, p { playerSelectedCard = Just card })) pId conns'
+  TVar.writeTVar conns adjusted
+
 convert :: P.Parser IncomingMessage
-convert = P.choice [playerConnectedParser, playersRequestParser]
+convert = P.choice [ playerConnectedParser
+                   , playersRequestParser
+                   , playerSelectedCardParser]
 
 playerConnectedParser :: P.Parser IncomingMessage
 playerConnectedParser = do
@@ -131,5 +170,11 @@ playersRequestParser :: P.Parser IncomingMessage
 playersRequestParser = do
   _ <- P.string "PLAYERS"
   return PlayersRequest
+
+playerSelectedCardParser :: P.Parser IncomingMessage
+playerSelectedCardParser = do
+  _ <- P.string "SELECTCARD"
+  _ <- P.char ':'
+  PlayerSelectedCard <$> P.decimal
 
 
